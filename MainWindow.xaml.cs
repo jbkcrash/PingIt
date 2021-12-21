e186aed64542bf7e4a9a71eb41935a5bbec039fd
James Kinney <jbkcrash@gmail.com>
2021-12-21 09:47:43 -0500

    Added system tray minimization, removed the application from the task tray, and made richTextBox output visualby better.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,6 +17,8 @@ using System.Net;
 using System.Net.NetworkInformation;
 using System.Windows.Media.Media3D;
 using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace PingIt
 {
@@ -30,6 +32,28 @@ namespace PingIt
     {
         DispatcherTimer pingTimer = new DispatcherTimer();
         IPAddress ipAddressCustom = IPAddress.Loopback;
+        NotifyIcon nIcon = new NotifyIcon();
+
+        bool bContinous = false;
+
+        int iTotalSent = 0;
+        int iTotalSuccess = 0;
+        int iThreshold = 100; // Threshold of missed responses to be considered critical
+
+        //Method to draw the ICMP meter.
+        public void DrawMeter()
+        {
+            //Line myLine = new Line();
+            //myLine.Stroke = System.Windows.Media.Brushes.LightSteelBlue;
+            //myLine.X1 = 1;
+            //myLine.X2 = reply.RoundtripTime;
+            //myLine.Y1 = 1;
+            //myLine.Y2 = reply.RoundtripTime;
+            //myLine.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+            //myLine.VerticalAlignment = System.Windows.VerticalAlignment.Center;
+            //myLine.StrokeThickness = 2;
+            //mainGrid.Children.Add(myLine);
+        }
 
         public void AddLine(string text)
         {
@@ -51,17 +75,71 @@ namespace PingIt
             byte[] buffer = Encoding.ASCII.GetBytes(data);
             int timeout = 120;
             PingReply reply = pingSender.Send(ipAddressCustom, timeout, buffer, options);
+            iTotalSent++; //That is one sent and replied
             if (reply.Status == IPStatus.Success)
             {
-                AddLine(String.Format("Address: {0}", reply.Address.ToString()));
-                AddLine(String.Format("RoundTrip time: {0}", reply.RoundtripTime));
-                AddLine(String.Format("Time to live: {0}", reply.Options.Ttl));
-                AddLine(String.Format("Don't fragment: {0}", reply.Options.DontFragment));
-                AddLine(String.Format("Buffer size: {0}", reply.Buffer.Length));
+                //TODO This model is ultimately a memory leak, there either needs to be a better way to do this console, or we need to groom the richtextbox,
+                //e.g. delete old paragraphs. One value of this model we can incorporate edit and save of the output. Plus this can be collapsed into one Run.
+
+                iTotalSuccess++; //That is one success
+
+                double dCurrent = (double)iTotalSuccess / (double)iTotalSent;
+
+                Run run = new Run(String.Format("Sent: {0}, Reply: {1}, {2:P2} Success ", iTotalSent, iTotalSuccess, dCurrent));
+                run.Background = new SolidColorBrush(Colors.Green);
+                Paragraph paragraph = new Paragraph(run);
+
+                run = new Run(String.Format("RTT: {0}, ", reply.RoundtripTime));
+                //run.Foreground = new SolidColorBrush(Colors.Red); // My Color
+                run.Background = new SolidColorBrush(Colors.Green);
+                paragraph.Inlines.Add(run);
+                //Paragraph paragraph = new Paragraph(run);
+
+                run = new Run(String.Format("TTL: {0}, ", reply.Options.Ttl));
+                run.Background = new SolidColorBrush(Colors.Green);
+                paragraph.Inlines.Add(run);
+
+                run = new Run(String.Format("DNF: {0}, ", reply.Options.DontFragment));
+                run.Background = new SolidColorBrush(Colors.Green);
+                paragraph.Inlines.Add(run);
+
+                run = new Run(String.Format("Buffer size: {0}", reply.Buffer.Length));
+                run.Background = new SolidColorBrush(Colors.Green);
+                paragraph.Inlines.Add(run);
+                
+                paragraph.FontSize = 20;
+ 
+                outputBox.Document.Blocks.Add(paragraph);
+
+                //AddLine(String.Format("Address: {0}", reply.Address.ToString()));
+                //AddLine(String.Format("RoundTrip time: {0}", reply.RoundtripTime));
+                //AddLine(String.Format("Time to live: {0}", reply.Options.Ttl));
+                //AddLine(String.Format("Don't fragment: {0}", reply.Options.DontFragment));
+                //AddLine(String.Format("Buffer size: {0}", reply.Buffer.Length));
+            } else
+            {
+                double dCurrent = (double)iTotalSuccess / (double)iTotalSent;
+
+                Run run = new Run(String.Format("Sent: {0}, Reply: {1}, {2:P2} Success ", iTotalSent, iTotalSuccess, dCurrent));
+                
+                run.Background = new SolidColorBrush(Colors.Red);
+                Paragraph paragraph = new Paragraph(run);
+
+                run = new Run("No response!");
+                run.Background = new SolidColorBrush(Colors.Red);
+
+                //paragraph = new Paragraph(run);
+                paragraph.Inlines.Add(run);
+                paragraph.FontSize = 20;
+                outputBox.Document.Blocks.Add(paragraph);
             }
+
+            // Keep RichTextBox at our last output...
+            outputBox.ScrollToEnd();
         }
         //fe80::d0a7:a507:5e23:d61c%18
 
+        //TODO need to strengthen IPV6 and make the output consistent.
         public void PingIPv6(string ipAddress)
         {
             Ping pingSender = new Ping();
@@ -121,6 +199,9 @@ namespace PingIt
         /// <param name="e"></param>
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            //Start with fresh metrics
+            iTotalSent = 0;
+            iTotalSuccess = 0;
 
             //First thing it so evaluate the address
             ipAddressCustom = IPAddress.Parse(ipaddress.Text);
@@ -257,6 +338,29 @@ namespace PingIt
             {
                 this.ipaddress.Text = ipAddressCustom.ToString();
             }
+        }
+        private void restoreWindow(object sender, EventArgs e)
+        {
+            Activate();
+            this.WindowState = WindowState.Normal; 
+            this.nIcon.Visible = false;
+        }
+
+        private void minimize(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = System.Windows.WindowState.Minimized;
+            this.nIcon.Icon = new Icon(@"../../systray.ico");
+            //this.nIcon.ShowBalloonTip(5000, "Hi", "This is a BallonTip from Windows Notification", ToolTipIcon.Info);
+            //this.AddHandler(this.nIcon.DoubleClick, new RoutedEventHandler(GetHandledToo), true);
+            this.nIcon.DoubleClick += new System.EventHandler(this.restoreWindow);
+
+            //Enable the Icon in Systray
+            this.nIcon.Visible = true;
+        }
+
+        private void closeWindow(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
